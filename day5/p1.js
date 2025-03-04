909d74de0ef219914a06f270ab48b80e1f9fd9b4
radhesatyam <satyamtiwari591@gmail.com>
2025-03-04 12:13:38 +0530

    express server

---
+++ day5/p1.js
@@ -0,0 +1,8 @@
+const express=require('express');
+const app = express();
+app.get('/',(req,res)=>{
+    res.send('Hello World');
+});
+app.listen(9000,()=>{
+    console.log('server is running on port 9000');
+})
